7aa13b992a0e809a9227dcc5b9f1655656bc3933
Yumegaarux <zaldarriagajanvictor@gmail.com>
2024-05-13 00:19:32 +0800

    trying to add calendar feature for choosing rental dates

--- src/classes/AddTransaction.java
+++ src/classes/AddTransaction.java
@@ -19,9 +19,7 @@ public class AddTransaction extends javax.swing.JFrame {
         DBcar = new javax.swing.JComboBox<>();
         jPanel4 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
-        TFdaterented = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        TFdatereturned = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         TFexpense = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
@@ -29,18 +27,20 @@ public class AddTransaction extends javax.swing.JFrame {
         jLabel8 = new javax.swing.JLabel();
         TFpayment = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
-        TFdo = new javax.swing.JTextField();
         jLabel10 = new javax.swing.JLabel();
-        jLabel11 = new javax.swing.JLabel();
         TFdest = new javax.swing.JTextField();
         TFpu = new javax.swing.JTextField();
         jLabel12 = new javax.swing.JLabel();
         TFcustname = new javax.swing.JTextField();
         jLabel13 = new javax.swing.JLabel();
         TFcustnum = new javax.swing.JTextField();
+        jDateChooser1 = new com.toedter.calendar.JDateChooser();
+        jDateChooser2 = new com.toedter.calendar.JDateChooser();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         BTNcancel = new javax.swing.JButton();
+        TFdo = new javax.swing.JTextField();
+        jLabel11 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -62,49 +62,51 @@ public class AddTransaction extends javax.swing.JFrame {
 
         jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel4.setText("Date Returned(YYYY/MM/DD):");
-        jPanel4.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 170, -1));
-        jPanel4.add(TFdaterented, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 10, 120, -1));
+        jLabel4.setText("Date Returned:");
+        jPanel4.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 0, 80, -1));
 
-        jLabel5.setText("Date Rented(YYYY/MM/DD):");
-        jPanel4.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 170, -1));
-        jPanel4.add(TFdatereturned, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 30, 120, -1));
+        jLabel5.setText("Date Rented:");
+        jPanel4.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 80, -1));
 
         jLabel3.setText("Payment:");
         jPanel4.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, -1, -1));
-        jPanel4.add(TFexpense, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 90, 90, -1));
+        jPanel4.add(TFexpense, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 100, 90, -1));
 
         jLabel6.setText("P");
-        jPanel4.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 90, -1, -1));
+        jPanel4.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 100, -1, -1));
 
         jLabel7.setText("P");
         jPanel4.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 70, -1, -1));
 
         jLabel8.setText("Expense:");
-        jPanel4.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 90, -1, -1));
+        jPanel4.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 100, -1, -1));
         jPanel4.add(TFpayment, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 70, 90, -1));
 
         jLabel9.setText("Destination: ");
-        jPanel4.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 130, -1, -1));
-        jPanel4.add(TFdo, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 190, 120, -1));
+        jPanel4.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 180, -1, -1));
 
         jLabel10.setText("Pick-up: ");
-        jPanel4.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 170, -1, -1));
+        jPanel4.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 210, -1, -1));
+        jPanel4.add(TFdest, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 180, 120, -1));
 
-        jLabel11.setText("Drop-off:");
-        jPanel4.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 190, -1, -1));
-        jPanel4.add(TFdest, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 130, 120, -1));
-        jPanel4.add(TFpu, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 170, 120, -1));
+        TFpu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TFpuActionPerformed(evt);
+            }
+        });
+        jPanel4.add(TFpu, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 210, 120, -1));
 
         jLabel12.setText("Customer Age:");
-        jPanel4.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 90, -1, -1));
-        jPanel4.add(TFcustname, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 70, 180, -1));
+        jPanel4.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 110, -1, -1));
+        jPanel4.add(TFcustname, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 70, 180, -1));
 
         jLabel13.setText("Customer Name:");
         jPanel4.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 70, -1, -1));
-        jPanel4.add(TFcustnum, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 90, 180, -1));
+        jPanel4.add(TFcustnum, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 110, 180, -1));
+        jPanel4.add(jDateChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 0, 110, -1));
+        jPanel4.add(jDateChooser2, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 0, 110, -1));
 
-        getContentPane().add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 150, 580, 230));
+        getContentPane().add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 160, 580, 230));
 
         jButton1.setText("Add");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -129,6 +131,10 @@ public class AddTransaction extends javax.swing.JFrame {
             }
         });
         getContentPane().add(BTNcancel, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 450, -1, -1));
+        getContentPane().add(TFdo, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 400, 120, -1));
+
+        jLabel11.setText("Drop-off:");
+        getContentPane().add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 400, -1, -1));
 
         pack();
         setLocationRelativeTo(null);
@@ -150,8 +156,8 @@ public class AddTransaction extends javax.swing.JFrame {
             car.setName(carDetails[2]);
             car.setModel(Integer.parseInt(carDetails[3]));
             
-            String dateRented = TFdaterented.getText();
-            String dateReturned = TFdatereturned.getText();
+            String dateRented = jDateChooser1.getDateFormatString();
+            String dateReturned = jDateChooser2.getDateFormatString();
             double payment = Double.parseDouble(TFpayment.getText());
             double expense = Double.parseDouble(TFexpense.getText());
             String pickUp = TFpu.getText();
@@ -162,8 +168,6 @@ public class AddTransaction extends javax.swing.JFrame {
             int rowsInserted = Connector.AddTransaction(hp.getCurrentUser().getID(), car.getPlate(), dateRented, dateReturned, payment, expense, pickUp, dropOff, customerName, customerNum);
             if (rowsInserted > 0){
                 JOptionPane.showMessageDialog(this, "Transaction Added Successfully!");
-                TFdaterented.setText("");
-                TFdatereturned.setText("");
                 TFpayment.setText("");
                 TFexpense.setText("");
                 TFpu.setText("");
@@ -190,8 +194,6 @@ public class AddTransaction extends javax.swing.JFrame {
     }//GEN-LAST:event_BTNcancelActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-    TFdaterented.setText("");
-    TFdatereturned.setText("");
     TFpayment.setText("");
     TFexpense.setText("");
     TFpu.setText("");
@@ -199,6 +201,10 @@ public class AddTransaction extends javax.swing.JFrame {
     TFcustname.setText("");
     TFcustnum.setText("");
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void TFpuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TFpuActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TFpuActionPerformed
     
     private void updateCombo(){
         try{
@@ -248,8 +254,6 @@ public class AddTransaction extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> DBcar;
     private javax.swing.JTextField TFcustname;
     private javax.swing.JTextField TFcustnum;
-    private javax.swing.JTextField TFdaterented;
-    private javax.swing.JTextField TFdatereturned;
     private javax.swing.JTextField TFdest;
     private javax.swing.JTextField TFdo;
     private javax.swing.JTextField TFexpense;
@@ -257,6 +261,8 @@ public class AddTransaction extends javax.swing.JFrame {
     private javax.swing.JTextField TFpu;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private com.toedter.calendar.JDateChooser jDateChooser1;
+    private com.toedter.calendar.JDateChooser jDateChooser2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
